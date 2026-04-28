dad11061aeefd424827d03b59b9670fd0eb79200
alex.tu <alex.tu@user.com>
2026-04-28 15:54:36 +0800

    feat: data_reader 添加 payment_type 字段查询

--- src/tools/data_reader.py
+++ src/tools/data_reader.py
@@ -278,6 +278,7 @@ def get_policy_by_city(
                 city.id AS city_id,
                 insc.code AS insurance_code, 
                 insc.name AS insurance_name, 
+                insc.payment_type as payment_type,
                 his.adjust_serial, 
                 his.start_time, 
                 his.min_radix, 
